e9d2509ac786ad015a3a7cc931c0c023e1d57522
Garves <garves44@gmail.com>
2020-05-19 10:21:01 -0600

    created taskhandler function in javasscript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,29 @@
 console.dir(window.document);
 
 
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 
 //button functionality
 var createTaskHandler = function () {
+    event.preventDefault();
+
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+    
+    //Create List Item
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task.";
+    //create div to hold task information and add to list item
+    var taskInfoEl = document.createElement("div");
+    //Give it a class Name
+    taskInfoEl.className = "task-info";
+    //Add HTML content to div
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+    listItemEl.appendChild(taskInfoEl);
+    //Add entire list item to list
     tasksToDoEl.appendChild(listItemEl);
 }
 
-buttonEl.addEventListener("click", createTaskHandler);
+formEl.addEventListener("submit", createTaskHandler);
