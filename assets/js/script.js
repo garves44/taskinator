cf012e2888e49f5c4bebd341350b8f1c6c7afadd
Garves <garves44@gmail.com>
2020-05-19 13:36:45 -0600

    added drag-drop feature to tasks

--- assets/js/script.js
+++ assets/js/script.js
@@ -172,8 +172,26 @@ var dropZoneDragHandler = function(event) {
     }
 
 };
+var dropTaskHandler = function(event){
+    var id = event.dataTransfer.getData("text/plain");
+    var draggableElement = document.querySelector("[data-task-id='" + id + "']");
+    var dropZoneEl = event.target.closest(".task-list");
+    var statusType = dropZoneEl.id;
+    var statusSelectEl = draggableElement.querySelector("select[name='status-change']");
+    
+    if(statusType === "tasks-to-do") {
+        statusSelectEl.selectedIndex = 0;
+    } else if (statusType === "tasks-in-progress"){
+        statusSelectEl.selectedIndex = 1;
+    } else if (statusType === "tasks-completed") { 
+        statusOptionEl.selectedIndex = 2;
+    }
+    
+    dropZoneEl.appendChild(draggableElement);
+};
 formEl.addEventListener("submit", taskFormHandler);
 pageContentEl.addEventListener("click", taskButtonHandler);
 pageContentEl.addEventListener("change", taskStatusChangeHandler);
 pageContentEl.addEventListener("dragstart", dragTaskHandler);
 pageContentEl.addEventListener("dragover", dropZoneDragHandler);
+pageContentEl.addEventListener("drop", dropTaskHandler);
