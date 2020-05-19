4351839ac2379e8d19f9cd4cc6a2e6f0d065a4dd
Garves <garves44@gmail.com>
2020-05-19 13:46:08 -0600

    added dragleave function

--- assets/js/script.js
+++ assets/js/script.js
@@ -170,6 +170,7 @@ var dropZoneDragHandler = function(event) {
     if (taskListEl) {
         event.preventDefault();
     }
+    taskListEl.setAttribute("style", "background: rgba(68, 233, 255, 0.7); border-style: dashed;");
 
 };
 var dropTaskHandler = function(event){
@@ -188,6 +189,15 @@ var dropTaskHandler = function(event){
     }
     
     dropZoneEl.appendChild(draggableElement);
+    dropZoneEl.removeAttribute("style");
+};
+var dragLeaveHandler = function(event) {
+    var taskListEl = event.target.closest(".task-list");
+    
+    if (taskListEl) {
+        taskListEl.removeAttribute("style");
+    }
+
 };
 formEl.addEventListener("submit", taskFormHandler);
 pageContentEl.addEventListener("click", taskButtonHandler);
@@ -195,3 +205,4 @@ pageContentEl.addEventListener("change", taskStatusChangeHandler);
 pageContentEl.addEventListener("dragstart", dragTaskHandler);
 pageContentEl.addEventListener("dragover", dropZoneDragHandler);
 pageContentEl.addEventListener("drop", dropTaskHandler);
+pageContentEl.addEventListener("dragleave", dragLeaveHandler);
